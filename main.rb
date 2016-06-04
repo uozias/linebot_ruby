395d92cee742a84c78856b9b1a83875316621b0d
AoyagiSaizo <uozias@gmail.com>
2016-06-04 19:22:21 +0900

    [Add] reveive json message with post

--- main.rb
+++ main.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require './line_message'
 
-get '/' do
-  from = params['from']
+configure do
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
+post '/' do
+  from = nil
+  params = JSON.parse request.body.read
+  from = params['result'][0]['from'] if params['result'].is_a? Array
   LineMessage.new.send(from)
-  'OK'
+  logger.info "from: " + from.to_s
+  'OK ' + from.to_s
 end
 
 
