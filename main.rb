8f0c178018fb9e6e3b991ef1156fa610405f80f3
AoyagiSaizo <uozias@gmail.com>
2016-06-04 19:36:37 +0900

    [Add] logging setting

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require './line_message'
 
 configure do
+  set :logging, Logger::DEBUG
   # logging is enabled by default in classic style applications,
   # so `enable :logging` is not needed
   file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
@@ -12,9 +13,10 @@ end
 post '/' do
   from = nil
   params = JSON.parse request.body.read
+  logger.debug params.to_s
   from = params['result'][0]['from'] if params['result'].is_a? Array
   LineMessage.new.send(from)
-  logger.info "from: " + from.to_s
+  logger.debug "from: " + from.to_s
   'OK ' + from.to_s
 end
 
