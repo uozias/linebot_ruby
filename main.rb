497d45ed7494cdc54d70f99f923df01a6bc0789c
AoyagiSaizo <uozias@gmail.com>
2016-06-04 18:29:37 +0900

    initial

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'dotenv'
+require 'faraday'
+require 'json'
+
+Dotenv.load
+
+conn = Faraday.new(:url => 'https://trialbot-api.line.me') do |faraday|
+  faraday.request  :url_encoded             # form-encode POST params
+  faraday.response :logger                  # log requests to STDOUT
+  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+end
+
+path = '/v1/events'
+body = {
+  to: [ENV["TARGET_MID"]],
+  toChannel: 1383378250,
+  eventType: "138311608800106203",
+  content: {
+    contentType: 1,
+    toType: 1,
+    text:"Hello, Jose!"
+  }
+}.to_json
+
+conn.post do |req|
+  req.url path
+  req.headers['Content-Type'] = 'application/json'
+  req.headers['X-Line-ChannelID'] =  ENV['LINE_CHANNEL_ID']
+  req.headers['X-Line-ChannelSecret'] = ENV['LINE_CHANNEL_SECRET']
+  req.headers['X-Line-Trusted-User-With-ACL'] = ENV['LINE_CHANNEL_MID']
+  req.body = body
+end
